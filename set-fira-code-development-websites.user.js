ceb69e2b1f5217333de42f8d844f7b5e614bf81b
Arylo <Arylo@users.noreply.github.com>
2026-09-15 09:01:24 +0000

    Deploying to monkey from @ arylo-scripts/monkey-set-fira-code-development-websites@b35e34641612c8254f97eaf1695e103bfce00495 🚀

--- set-fira-code-development-websites.user.js
+++ set-fira-code-development-websites.user.js
@@ -2,79 +2,74 @@
 // @name Set Fira Code as font in development websites
 // @name:en Set Fira Code as font in development websites
 // @name:zh Fira Code 作为网站的代码字体
+// @name:zh-CN Fira Code 作为网站的代码字体
 // @name:zh-Hans Fira Code 作为网站的代码字体
 // @name:zh-Hant Fira Code 作為網站的代碼字體
-// @name:zh-CN Fira Code 作为网站的代码字体
 // @name:zh-HK Fira Code 作為網站的代碼字體
 // @name:zh-MO Fira Code 作為網站的代碼字體
-// @name:zh-TW Fira Code 作為網站的代碼字體
-// @name:zh-SG Fira Code 作为网站的代码字体
 // @name:zh-MY Fira Code 作为网站的代码字体
+// @name:zh-SG Fira Code 作为网站的代码字体
+// @name:zh-TW Fira Code 作為網站的代碼字體
 // @description Unified use of `Fira Code` as the code font to enhance developers' awareness of the code on the page
 // @description:en Unified use of `Fira Code` as the code font to enhance developers' awareness of the code on the page
 // @description:zh 统一将`Fira Code`作为代码字体, 提高开发者对页面中的代码的感知度
+// @description:zh-CN 统一将`Fira Code`作为代码字体, 提高开发者对页面中的代码的感知度
 // @description:zh-Hans 统一将`Fira Code`作为代码字体, 提高开发者对页面中的代码的感知度
 // @description:zh-Hant 統一將`Fira Code`作為代碼字體, 提高開發者對頁面中代碼的感知度
-// @description:zh-CN 统一将`Fira Code`作为代码字体, 提高开发者对页面中的代码的感知度
 // @description:zh-HK 統一將`Fira Code`作為代碼字體, 提高開發者對頁面中代碼的感知度
 // @description:zh-MO 統一將`Fira Code`作為代碼字體, 提高開發者對頁面中代碼的感知度
-// @description:zh-TW 統一將`Fira Code`作為代碼字體, 提高開發者對頁面中代碼的感知度
-// @description:zh-SG 统一将`Fira Code`作为代码字体, 提高开发者对页面中的代码的感知度
 // @description:zh-MY 统一将`Fira Code`作为代码字体, 提高开发者对页面中的代码的感知度
-// @version 15
+// @description:zh-SG 统一将`Fira Code`作为代码字体, 提高开发者对页面中的代码的感知度
+// @description:zh-TW 統一將`Fira Code`作為代碼字體, 提高開發者對頁面中代碼的感知度
+// @version 16
 // @author Arylo
-// @include https://webpack.js.org/*
-// @include https://rollupjs.org/*
+// @include https://*.github.io/*
+// @include https://ajv.js.org/*
+// @include https://axios-http.com/*
+// @include https://developers.weixin.qq.com/miniprogram/*
+// @include https://docs.docker.com/*
+// @include https://docs.gitlab.com/*
+// @include https://docs.nestjs.com/*
+// @include https://docs.npmjs.com/*
+// @include https://docs.taro.zone/*
+// @include https://eslint.org/*
+// @include https://greasyfork.org/*/code
 // @include https://jestjs.io/*
+// @include https://lodash.com/*
+// @include https://ls-lint.org/*
+// @include https://mochajs.org/*
+// @include https://nextjs.org/*
+// @include https://nodejs.org/docs/*
+// @include https://nodemailer.com/*
+// @include https://npmjs.com/*
+// @include https://pnpm.io/*
+// @include https://react.dev/*
+// @include https://rollupjs.org/*
+// @include https://rxjs.dev/*
+// @include https://toml.io/*
 // @include https://turbo.build/*
 // @include https://vite.dev/*
 // @include https://vitest.dev/*
-// @include https://lodash.com/*
-// @include https://docs.taro.zone/*
-// @include https://ajv.js.org/*
-// @include https://yargs.js.org/*
-// @include https://www.tampermonkey.net/*
-// @include https://*.github.io/*
-// @include https://docs.gitlab.com/*
-// @include https://www.w3schools.com/*
-// @include https://www.typescriptlang.org/*
-// @include https://yarnpkg.com/*
-// @include https://pnpm.io/*
-// @include https://npmjs.com/*
-// @include https://docs.npmjs.com/*
-// @include https://nodejs.org/docs/*
 // @include https://vuejs.org/api/*
 // @include https://vueuse.org/*
-// @include https://react.dev/*
-// @include https://rxjs.dev/*
-// @include https://axios-http.com/*
-// @include https://nextjs.org/*
-// @include https://docs.nestjs.com/*
-// @include https://eslint.org/*
-// @include https://mochajs.org/*
-// @include https://toml.io/*
-// @include https://ls-lint.org/*
-// @include https://nodemailer.com/*
-// @include https://greasyfork.org/*/code
-// @include https://docs.docker.com/*
-// @include https://developers.weixin.qq.com/miniprogram/*
+// @include https://webpack.js.org/*
+// @include https://www.tampermonkey.net/*
+// @include https://www.typescriptlang.org/*
+// @include https://www.w3schools.com/*
+// @include https://yargs.js.org/*
+// @include https://yarnpkg.com/*
 // @license MIT
+// @originalName set-fira-code-development-websites
 // @resource font_css https://cdn.jsdelivr.net/npm/firacode@6.2.0/distr/fira_code.css
-// @homepage https://github.com/Arylo/scripts#readme
-// @supportURL https://github.com/Arylo/scripts/issues
-// @downloadURL https://raw.githubusercontent.com/Arylo/scripts/monkey/set-fira-code-development-websites.user.js
-// @updateURL https://raw.githubusercontent.com/Arylo/scripts/monkey/set-fira-code-development-websites.meta.js
 // @run-at document-idle
 // @grant GM_addStyle
 // @grant GM_getResourceText
 // ==/UserScript==
 "use strict";
 (() => {
-  // ../../../packages/monkey/gm-polyfill/GM_addStyle.ts
+  // node_modules/@arylo-scripts/gm-polyfill/dist/index.mjs
   function GM_addStyle(...args) {
-    if (typeof window.GM_addStyle === "function") {
-      return window.GM_addStyle(...args);
-    }
+    if (typeof window.GM_addStyle === "function") return window.GM_addStyle(...args);
     const [cssContent] = args;
     const head = document.getElementsByTagName("head")[0];
     if (head) {
@@ -86,12 +81,8 @@
     }
     return null;
   }
-
-  // ../../../packages/monkey/gm-polyfill/GM_getResourceText.ts
   function GM_getResourceText(...args) {
-    if (typeof window.GM_getResourceText !== "function") {
-      throw new Error("GM_getResourceText is not available");
-    }
+    if (typeof window.GM_getResourceText !== "function") throw new Error("GM_getResourceText is not available");
     return window.GM_getResourceText(...args);
   }
 
@@ -99,15 +90,8 @@
   var template_default = '*{font-family:Fira Code,monospace!important;font-variant-ligatures:contextual;font-feature-settings:"calt"}\n';
 
   // index.ts
-  var DEFAULT_PARENT_SELECTORS = [
-    ":not(li) > a",
-    ":not(h1):not(h2):not(h3):not(h4):not(h5) >"
-  ];
-  var DEFAULT_CODE_SELECTORS = [
-    "code",
-    "code *",
-    "pre:not(:has(code))"
-  ];
+  var DEFAULT_PARENT_SELECTORS = [":not(li) > a", ":not(h1):not(h2):not(h3):not(h4):not(h5) >"];
+  var DEFAULT_CODE_SELECTORS = ["code", "code *", "pre:not(:has(code))"];
   function parseSelectors(selectors, parentSelectors) {
     const realSelectors = selectors.reduce((list, s) => {
       for (const ps of parentSelectors) {
@@ -121,7 +105,10 @@
     return template_default.replace(/\*/g, selectors.join(", "));
   }
   setTimeout(async () => {
-    const fontCssContent = (await GM_getResourceText("font_css")).replace(/(\burl\(["'])/g, "$1https://cdn.jsdelivr.net/npm/firacode@6.2.0/distr/");
+    const fontCssContent = (await GM_getResourceText("font_css")).replace(
+      /(\burl\(["'])/g,
+      "$1https://cdn.jsdelivr.net/npm/firacode@6.2.0/distr/"
+    );
     GM_addStyle(fontCssContent);
     const codeSelectors = DEFAULT_CODE_SELECTORS;
     const parentSelectors = DEFAULT_PARENT_SELECTORS;
